9fe2419549b4bb92941bd50af65888683ed1f2e8
Ryan Barrett <git@ryanb.org>
2012-06-09 09:59:33 -0700

    finished moving envelope code to source.py

--- source.py
+++ source.py
@@ -8,6 +8,39 @@ TODO:
 __author__ = ['Ryan Barrett <salmon@ryanb.org>']
 
 import datetime
+try:
+  import json
+except ImportError:
+  import simplejson as json
+
+import appengine_config
+from webutil import util
+from django_salmon import magicsigs
+from django_salmon import utils
+
+
+# temporary URL for fetching magic sig private keys from webfinger-unofficial
+USER_KEY_HANDLER = \
+    'https://facebook-webfinger.appspot.com/user_key?uri=%s&secret=%s'
+
+# Templates for Atom Salmons and Magic Envelopes. Note that the format
+# specifiers have mapping keys. Used in comment_to_salmon().
+ATOM_SALMON_TEMPLATE = """\
+<?xml version='1.0' encoding='UTF-8'?>
+<entry xmlns='http://www.w3.org/2005/Atom'>
+  <id>%(id_tag)s</id>
+  <author>
+    <name>%(author_name)s</name>
+    <uri>%(author_uri)s</uri>
+  </author>
+  <thr:in-reply-to xmlns:thr='http://purl.org/syndication/thread/1.0'
+    ref='%(in_reply_to_tag)s'>
+    %(in_reply_to_tag)s
+  </thr:in-reply-to>
+  <content>%(content)s</content>
+  <title>%(title)s</title>
+  <updated>%(updated)s</updated>
+</entry>"""
 
 
 class Source(object):
@@ -40,3 +73,24 @@ class Source(object):
     Background on tag URIs: http://taguri.org/
     """
     return 'tag:%s,%d:%s' % (self.DOMAIN, datetime.datetime.now().year, name)
+
+  def envelope(self, salmon, author_uri):
+    """Signs and encloses an Atom Salmon in a Magic Signature envelope.
+
+    Fetches the author's Magic Signatures public key via LRDD in order to create
+    the signature.
+
+    Args:
+      salmon: string, an Atom Salmon
+      author_uri: string, the author's URI, beginning with acct:
+
+    Returns: JSON dict following Magic Signatures spec section 3.5:
+    http://salmon-protocol.googlecode.com/svn/trunk/draft-panzer-magicsig-01.html#anchor5
+    """
+    class Struct(object):
+      def __init__(self, **kwargs):
+        vars(self).update(**kwargs)
+
+    key_url = USER_KEY_HANDLER % (author_uri, appengine_config.USER_KEY_HANDLER_SECRET)
+    key = Struct(**json.loads(util.urlfetch(key_url)))
+    return magicsigs.magic_envelope(salmon, 'application/atom+xml', key)
