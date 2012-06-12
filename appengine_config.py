cb26ecf8b2746b76efc4c71423ee03d3ca7563e1
Ryan Barrett <git@ryanb.org>
2012-06-11 22:31:40 -0700

    switch from symlinks to editing sys.path for importing libraries from git submodules

--- appengine_config.py
+++ appengine_config.py
@@ -1,6 +1,13 @@
 """App Engine settings.
 """
 
+import sys
+import os
+
+# git submodules to allow importing from
+for submodule in 'django-salmon', 'google-api-python-client', 'python-gflags':
+  sys.path.append(os.path.join(os.path.abspath("."), submodule))
+
 from webutil.appengine_config import *
 
 USER_KEY_HANDLER_SECRET = read('user_key_handler_secret')
